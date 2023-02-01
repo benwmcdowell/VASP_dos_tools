8b429af97e6a1ba2038f1807389a63e455a843f9
Benjamin McDowell <71850608+benwmcdowell@users.noreply.github.com>
2023-02-01 10:44:28 -0800

    Update plot_eigenval.py
    Fixed error in plotting of dispersion curves

--- VASP_dos_tools/plot_eigenval.py
+++ VASP_dos_tools/plot_eigenval.py
@@ -1,6 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import sys
+import os
 
 def parse_eigenval(ifile):
     with open(ifile) as file:
@@ -13,8 +13,9 @@ def parse_eigenval(ifile):
         for j in range(nkpts):
             for i in range(2):
                 line=file.readline()
-            kpts[i+j*nstates]=np.array([float(k) for k in line.split()[:3]])
+            current_k=np.array([float(k) for k in line.split()[:3]])
             for i in range(nstates):
+                kpts[i+j*nstates]=current_k
                 line=file.readline().split()
                 if len(line)==5:
                     #if calc is spin polarized
@@ -40,17 +41,29 @@ def plot_eigenval(ifile,**args):
     axs.hist(eigenval,bins=nbins)
     fig.show()
     
-def plot_dispersion(ifile,kvec):
-    eigenval,nstates,kpts=parse_eigenval(ifile)
-    e=[]
-    k=[]
-    for i,j in zip(kpts,eigenval):
-        if np.dot(i,kvec)/np.linalg.norm(i)/np.linalg.norm(kvec)==1:
-            e.append(j)
-            k.append(np.dot(i,kvec)/np.linalg.norm(kvec))
+def plot_dispersion(fpath,kvec):
+    os.chdir(fpath)
+    eigenval,nstates,kpts=parse_eigenval('./EIGENVAL')
+    lv=parse_poscar('./POSCAR')[0]
+    ef=parse_doscar('./DOSCAR')
+    
+    kvec=np.dot(kvec,lv)
+    eigenval-=ef
+    
+    e=np.zeros((nstates,int(len(kpts)/nstates)))
+    k=np.zeros((np.shape(e)[1],3))
+    for i in range(nstates):
+        for j in range(np.shape(e)[1]):
+            if i==0:
+                k[j]=np.dot(kpts[j*nstates+i],kvec)/np.linalg.norm(kvec)
+            e[i,j]=eigenval[j*nstates+i]
+            
     plt.figure()
-    plt.scatter(k,e)
+    for i in range(nstates):
+        plt.plot(k,e[i])
     plt.show()
+    
+    return k,e
     
 #reads DOSCAR
 def parse_doscar(filepath):
@@ -61,3 +74,36 @@ def parse_doscar(filepath):
         ef=float(line[3])
     return ef
 
+#reads POSCAR
+def parse_poscar(ifile):
+    with open(ifile, 'r') as file:
+        lines=file.readlines()
+        sf=float(lines[1])
+        latticevectors=[float(lines[i].split()[j])*sf for i in range(2,5) for j in range(3)]
+        latticevectors=np.array(latticevectors).reshape(3,3)
+        atomtypes=lines[5].split()
+        atomnums=[int(i) for i in lines[6].split()]
+        if 'Direct' in lines[7] or 'Cartesian' in lines[7]:
+            start=8
+            mode=lines[7].split()[0]
+        else:
+            mode=lines[8].split()[0]
+            start=9
+            seldyn=[''.join(lines[i].split()[-3:]) for i in range(start,sum(atomnums)+start)]
+        coord=np.array([[float(lines[i].split()[j]) for j in range(3)] for i in range(start,sum(atomnums)+start)])
+        if mode!='Cartesian':
+            for i in range(sum(atomnums)):
+                for j in range(3):
+                    while coord[i][j]>1.0 or coord[i][j]<0.0:
+                        if coord[i][j]>1.0:
+                            coord[i][j]-=1.0
+                        elif coord[i][j]<0.0:
+                            coord[i][j]+=1.0
+                coord[i]=np.dot(coord[i],latticevectors)
+            
+    #latticevectors formatted as a 3x3 array
+    #coord holds the atomic coordinates with shape ()
+    try:
+        return latticevectors, coord, atomtypes, atomnums, seldyn
+    except NameError:
+        return latticevectors, coord, atomtypes, atomnums
